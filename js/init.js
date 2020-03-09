0fc479455d7f517d5db7a992d2848c68e5ac908a
Marcelo A Tabone <marceloatabone@hotmail.com>
2020-03-09 14:41:29 -0300

    Creating a website base
    Creating the basis of a used website:
       - FrameWork Materialize

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(function(){
+  $("#header").load("views/includes/header.html"); 
+});
+
+$(function(){
+  $("#footer").load("views/includes/footer.html"); 
+});
+
+
+
+
